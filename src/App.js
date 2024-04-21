33a048379f36b06bb5f085e89fa085321a2615da
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-04-21 22:36:24 +0200

    Add App component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+// src/App.js
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import Header from './components/Header/Header.js';
+import Login from './components/Login/Login.js';
+import Footer from "./components/Footer/Footer";
+import Notifications from "./components/Notifications/Notifications";
+import AccountManagement from "./components/AccountManagement/AccountManagement";
+import ErrorManagement from "./components/ErrorManagement/ErrorManagement";
+import EventManagement from "./components/EventManagement/EventManagement";
 import './App.css';
+import GroupManagement from "./components/GroupManagement/GroupManagement"; // Import the CSS file
+
+function MainContent() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const location = useLocation();
+
+    const handleLogin = () => {
+        setIsLoggedIn(true);
+    };
+
+    const handleLogout = () => {
+        setIsLoggedIn(false);
+    };
+
+    if (!isLoggedIn) {
+        return <Login onLogin={handleLogin} />;
+    }
+
+    return (
+        <div className="App">
+            {location.pathname !== "/login" && <Header onLogout={handleLogout} />}
+            <div className='content'>
+                <Routes>
+                    <Route path="/account-management" element= { <AccountManagement /> } > </Route>
+                    <Route path="/group-management" element={ <GroupManagement /> }> </Route>
+                    <Route path="/event-management" element={ <EventManagement /> }> </Route>
+                    <Route path="/notifications" element= { <Notifications/> } > </Route>
+                    <Route path="/error-reports" element={ <ErrorManagement />}> </Route>
+                    <Route path="/login" element={ <Login onLogin={handleLogin} />}> </Route>
+                    <Route path="/"  element = { <AccountManagement />}> </Route>
+                </Routes>
+            </div>
+            {location.pathname !== "/login" && <Footer />}
+        </div>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <MainContent />
+        </Router>
+    );
 }
 
 export default App;
