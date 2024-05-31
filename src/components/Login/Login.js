a4dc7da4a8b963c6a427d17d19f78f1e8c3c0dab
JagodaJakub <kubajagjag@gmail.com>
2024-05-31 23:15:58 +0200

    Added email and password storing in variables and post to backend

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import './Login.css'; // Import the CSS file
+import React, {useState, useEffect} from 'react';
+import {useNavigate} from 'react-router-dom';
+import './Login.css';
 import logo from '../../images/logo.png';
-import ResetPassword from "../ResetPassword/ResetPassword"; // Import the logo image
+import ResetPassword from "../ResetPassword/ResetPassword";
 import EmailConfirmation from "../EmailConfirmation/EmailConfirmation";
+import axios from "axios";
 
-function Login({ onLogin, onLogout }) {
+function Login({onLogin, onLogout}) {
     const navigate = useNavigate();
     const [resetClicked, setResetClicked] = useState(false);
     const [resetEmail, setResetEmail] = useState(false);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
     useEffect(() => {
         if (onLogin) {
@@ -16,10 +19,28 @@ function Login({ onLogin, onLogout }) {
         }
     }, [onLogin]);
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault();
-        onLogin();
-        navigate("/");
+        try {
+            const response = await axios.post('/auth/authenticate', {email, password});
+            if (response.status === 200) {
+                localStorage.setItem('token', response.data.token);
+                onLogin();
+                navigate("/");
+            } else {
+                console.error('Login failed:', response.data.message);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    };
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
     };
 
     const handleEmailConfirm = () => {
@@ -27,52 +48,67 @@ function Login({ onLogin, onLogout }) {
         setResetClicked(true);
     }
 
-
     const handleResetPassword = () => {
         setResetEmail(true);
         onLogout();
         navigate("/email-confirmation");
     }
 
-    if(resetEmail) {
-        return <EmailConfirmation onConfirm={handleEmailConfirm} />;
+    if (resetEmail) {
+        return <EmailConfirmation onConfirm={handleEmailConfirm}/>;
     }
 
     if (resetClicked) {
-        return <ResetPassword onReset={() => {}} />;
+        return <ResetPassword onReset={() => {
+        }}/>;
     }
 
-    return (
-        <div className="d-flex flex-column justify-content-center align-items-center login-container">
-            <div>
-                <img style={{paddingLeft: '25px'}} src={logo} alt="Logo"/>
-                <h1 className="text-center" style={{color: 'var(--black-purple)'}}> Timeder</h1>
-            </div>
-            <div className="card" style={{width: '30rem', height: '30rem', borderRadius: '50px'}}>                <div className="card-body">
-                    <div className="icon-container">
-                        <i className='bi bi-person-circle icon'></i>
-                    </div>
-                    <h3 className="text-center">Sign in</h3>
-                    <form onSubmit={handleSubmit}>
-                        <div className="form-group input-icon">
-                            <i className="fas fa-envelope"></i>
-                            <input type="email" className="form-control" id="inputEmail" placeholder="Email" />
-                        </div>
-                        <div className="form-group input-icon">
-                            <i className="fas fa-lock"></i>
-                            <input type="password" className="form-control" id="inputPassword" placeholder="Password" />
-                        </div>
-                        <div className="form-group">
-                            <button type="submit" className="btn btn-purple btn-block">Sign in</button>
-                        </div>
-                        <div className="form-group text-right">
-                            <button type="button" className="btn btn-link btn-sm" onClick={handleResetPassword}>Forgot Password?</button>
-                        </div>
-                    </form>
+    return (<div className="d-flex flex-column justify-content-center align-items-center login-container">
+        <div>
+            <img style={{paddingLeft: '25px'}} src={logo} alt="Logo"/>
+            <h1 className="text-center" style={{color: 'var(--black-purple)'}}> Timeder</h1>
+        </div>
+        <div className="card" style={{width: '30rem', height: '30rem', borderRadius: '50px'}}>
+            <div className="card-body">
+                <div className="icon-container">
+                    <i className='bi bi-person-circle icon'></i>
                 </div>
+                <h3 className="text-center">Sign in</h3>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group input-icon">
+                        <i className="fas fa-envelope"></i>
+                        <input
+                            type="email"
+                            className="form-control"
+                            id="inputEmail"
+                            placeholder="Email"
+                            value={email}
+                            onChange={handleEmailChange}
+                        />
+                    </div>
+                    <div className="form-group input-icon">
+                        <i className="fas fa-lock"></i>
+                        <input
+                            type="password"
+                            className="form-control"
+                            id="inputPassword"
+                            placeholder="Password"
+                            value={password}
+                            onChange={handlePasswordChange}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <button type="submit" className="btn btn-purple btn-block">Sign in</button>
+                    </div>
+                    <div className="form-group text-right">
+                        <button type="button" className="btn btn-link btn-sm" onClick={handleResetPassword}>Forgot
+                            Password?
+                        </button>
+                    </div>
+                </form>
             </div>
         </div>
-    );
+    </div>);
 }
 
 export default Login;
