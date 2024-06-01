959175676aaa7f683ef5686f603b4564b0c778a2
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-06-02 00:36:11 +0200

    Add auth and group axios service

---
+++ src/services/authService.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { jwtDecode } from 'jwt-decode';
+
+const AuthService = {
+    getToken: function() {
+        return localStorage.getItem('token');
+    },
+
+    decodeToken: function() {
+        const token = this.getToken();
+        return jwtDecode(token);
+    },
+
+    getUserId: function() {
+        const decoded = this.decodeToken();
+        return decoded.userId;
+    },
+
+    setAxiosAuthHeader: function() {
+        const token = this.getToken();
+        if (token) {
+            axios.defaults.headers.common['Authorization'] = 'Bearer ' + token;
+        } else {
+            delete axios.defaults.headers.common['Authorization'];
+        }
+    }
+};
+
+export default AuthService;
